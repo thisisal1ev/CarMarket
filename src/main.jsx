2fca1b470db8de7fea66c19653e74fdb7212e081
thisisal1ev <aaalievvv1@gmail.com>
2024-06-17 12:11:50 +0500

    added NextUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -7,6 +9,8 @@ import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </Provider>
 );
